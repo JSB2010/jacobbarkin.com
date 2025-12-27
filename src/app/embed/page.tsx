8fd151f871ccf8e45845b334e1e489d2426bdd16
Jacob Barkin <jacobsamuelbarkin@gmail.com>
2025-12-26 18:04:29 -0600

    Embed page instructions fix

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -2,9 +2,17 @@ import fs from "node:fs/promises";
 import path from "node:path";
 import EmbedPageClient from "./EmbedPageClient";
 
+export const dynamic = "force-static";
+
 export default async function EmbedPage() {
   const instructionsPath = path.join(process.cwd(), "public", "embed", "INSTRUCTIONS.md");
-  const instructionsText = await fs.readFile(instructionsPath, "utf8");
+  let instructionsText = "";
+
+  try {
+    instructionsText = await fs.readFile(instructionsPath, "utf8");
+  } catch {
+    instructionsText = "";
+  }
 
   return <EmbedPageClient instructionsText={instructionsText} />;
 }
